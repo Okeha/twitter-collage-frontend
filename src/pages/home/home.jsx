7dfba0bb2133a9fc8cb6564310fcbb5ef6c6f8a6
Okeha <okehanthony1234@gmail.com>
2024-05-26 17:40:54 +0100

    Update: Logic to handle unifying files before sending to API changed

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,53 +3,28 @@ import Space from "../../components/space";
 import "./home.css";
 
 export default function Home() {
-  const [selectedMainFile, setSelectedMainFile] = useState(null);
-  const [selectedSideFiles, setSelectedSideFiles] = useState(null);
-
   const [loading, setLoading] = useState(false);
 
-  const [unifiedFile, setUnifiedFile] = useState([]);
-
-  const handleMainFileChange = (event) => {
-    const file = event.target.files[0];
-    setSelectedMainFile(file);
-  };
-
-  const handleSideFilesChange = (event) => {
-    const files = Array.from(event.target.files); // Convert FileList to array
-    setSelectedSideFiles(files);
-  };
-
-  const handleSetUnifierFile = async (file) => {
-    setUnifiedFile(file);
-  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
 
-    const file = [];
+    const mainFile = e.target.mainFile.files[0];
+    const sideFiles = e.target.sideFiles.files;
 
-    file.push(selectedMainFile);
-    file.push(...selectedSideFiles);
+    var fullFileList = [mainFile, ...sideFiles];
 
-    await handleSetUnifierFile(file);
-
-    // console.log(unifiedFile);
-    // alert("Yo");
-
-    // Assuming your API endpoint is at 'http://localhost:3000/upload'
     const formData = new FormData();
-
-    for (const file of unifiedFile) {
+    for (const file of fullFileList) {
       formData.append("images", file); // Adjust field name if needed
     }
 
     try {
-      // const url = `http://localhost:3005/api/v1/imageProcess/collage`;
+      const url = `http://localhost:3005/api/v1/imageProcess/collage`;
 
       // const url = `https://twitter-image-collager.osc-fr1.scalingo.io/api/v1/imageProcess/collage`;
 
-      const url = `https://twitter-collage-backend.onrender.com/api/v1/imageProcess/collage`;
+      // const url = `https://twitter-collage-backend.onrender.com/api/v1/imageProcess/collage`;
 
       console.log(url);
       const response = await fetch(url, {
@@ -128,8 +103,8 @@ export default function Home() {
                   type="file"
                   accept="image/*"
                   required
-                  id="files"
-                  onChange={handleMainFileChange}
+                  id="mainFile"
+                  name="mainFile"
                 ></input>
               </div>
             </div>
@@ -144,8 +119,8 @@ export default function Home() {
                   accept="image/*"
                   multiple
                   required
-                  id="files"
-                  onChange={handleSideFilesChange}
+                  id="sideFiles"
+                  name="sideFiles"
                 ></input>
               </div>
             </div>
